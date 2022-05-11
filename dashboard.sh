959a0a922809eacce9bbc54a13e9ecbb167d9d38
pc1 <jearangoo@unal.edu.co>
2022-05-11 09:59:03 -0500

    fix dashboard for kubernetes 1.24

--- dashboard.sh
+++ dashboard.sh
@@ -1,16 +1,16 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.1/aio/deploy/recommended.yaml
 kubectl apply -f admin-kube-dashboard.yaml
 
-PATCH='[{"op":"add", "path":"/spec/template/spec/tolerations/-", "value":{"key":"app/app-reserved","effect":"NoSchedule","operator":"Exists"}}]'
-kubectl patch deployment kubernetes-dashboard --patch "${PATCH}" --type json -n kubernetes-dashboard
-kubectl patch deployment dashboard-metrics-scraper --patch "${PATCH}" --type json -n kubernetes-dashboard
+#PATCH='[{"op":"add", "path":"/spec/template/spec/tolerations/-", "value":{"key":"app/app-reserved","effect":"NoSchedule","operator":"Exists"}}]'
+#kubectl patch deployment kubernetes-dashboard --patch "${PATCH}" --type json -n kubernetes-dashboard
+#kubectl patch deployment dashboard-metrics-scraper --patch "${PATCH}" --type json -n kubernetes-dashboard
 
 echo
 
 echo COPY TOKEN
-kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
+kubectl -n kubernetes-dashboard create token admin-user
 
-echo
+sleep 5
 echo
 URL=http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/overview?namespace=_all
 echo OPEN ${URL} && open ${URL}
