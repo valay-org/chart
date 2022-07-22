5d9de87769dd6914cca2e9bb9c84c0b98a48c6f2
pc2 <jearangoo@unal.edu.co>
2022-07-22 11:43:04 -0500

    fixes

--- dashboard.sh
+++ dashboard.sh
@@ -1,15 +1,10 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.1/aio/deploy/recommended.yaml
 kubectl apply -f admin-kube-dashboard.yaml
-
-#PATCH='[{"op":"add", "path":"/spec/template/spec/tolerations/-", "value":{"key":"app/app-reserved","effect":"NoSchedule","operator":"Exists"}}]'
-#kubectl patch deployment kubernetes-dashboard --patch "${PATCH}" --type json -n kubernetes-dashboard
-#kubectl patch deployment dashboard-metrics-scraper --patch "${PATCH}" --type json -n kubernetes-dashboard
-
 echo
-
 echo COPY TOKEN
+echo
 kubectl -n kubernetes-dashboard create token admin-user
-
+echo
 echo opening...
 sleep 5
 echo
